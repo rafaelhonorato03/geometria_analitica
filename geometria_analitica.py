5f7c3a993a873df6580fe4cdb3795a8f340c1907
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-03-31 20:22:44 -0300

    Update geometria_analitica.py

--- geometria_analitica.py
+++ geometria_analitica.py
@@ -5,3 +5,7 @@ vetor_u = np.array([1, -2, 0])
 
 # Declarando vetor v
 vetor_v = np.array([0, 2, 2])
+
+# Soma de vetores
+soma_vetores = vetor_u + vetor_v
+print("Soma dos vetores u e v: ", soma_vetores)
