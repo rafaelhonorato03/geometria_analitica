9a5e7ddfc77604074cb530344aeca412c3c8f59a
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-03-31 20:24:38 -0300

    Update geometria_analitica.py

--- geometria_analitica.py
+++ geometria_analitica.py
@@ -9,3 +9,15 @@ vetor_v = np.array([0, 2, 2])
 # Soma de vetores
 soma_vetores = vetor_u + vetor_v
 print("Soma dos vetores u e v: ", soma_vetores)
+
+# Multiplicaçao de vetor com escalar
+multiplicacao_escalar = 7 * vetor_u
+print("Multiplicação do vetor u por 7: ", multiplicacao_escalar)
+
+# Produto escalar de u com v
+produto_escalar = np.dot(vetor_u, vetor_v)
+print("Produto escalar de u e v: ", produto_escalar)
+
+# Produto vetorial de u com v
+produto_vetorial = np.cross(vetor_u, vetor_v)
+print("Produto vetorial de u e v: ", produto_vetorial)
