aa5ebc861abc67f858588026d6996d2d066c1d77
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-03-31 20:27:24 -0300

    Update geometria_analitica.py

--- geometria_analitica.py
+++ geometria_analitica.py
@@ -6,6 +6,9 @@ vetor_u = np.array([1, -2, 0])
 # Declarando vetor v
 vetor_v = np.array([0, 2, 2])
 
+# Declarando o vetor w
+vetor_w = np.array([1, 2, -1])
+
 # Soma de vetores
 soma_vetores = vetor_u + vetor_v
 print("Soma dos vetores u e v: ", soma_vetores)
@@ -21,3 +24,8 @@ print("Produto escalar de u e v: ", produto_escalar)
 # Produto vetorial de u com v
 produto_vetorial = np.cross(vetor_u, vetor_v)
 print("Produto vetorial de u e v: ", produto_vetorial)
+
+# Produto misto de u, v e w
+produto_misto = np.linalg.det([
+    vetor_u, vetor_v, vetor_w])
+print("Produto misto de u, v e w: ", produto_misto)
